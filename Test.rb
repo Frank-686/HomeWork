385da8f31dab72410442ae72c48620eefb90f14a
biaohua <biaohua.li@lcola.cn>
2016-06-02 19:06:10 -0700

    haaa

--- Test.rb
+++ Test.rb
@@ -7,20 +7,21 @@ require 'json'
 def test
   response = YAML.load(File.open("yaml.yml"))
   puts "查询字符串#{"a"}"
-    response.each do |k,v|
-      ss = Thread.new{
-        url_response = Net::HTTP.get(URI.parse(v))
-        File.new('test.json', "w+").syswrite(url_response)
-        puts "------#{k}-----"
-        js_response = JSON.parse(File.read('test.json'))
-        # puts js_response.class
-          js_response.each do |a,b|
-            if b.to_s["a"]
-              puts "Key: #{a} ; Value: #{b}"
-            end
-          end
-      }
-      ss.join
-    end
+  response.each do |k,v|
+    ss = Thread.new{
+      url_response = Net::HTTP.get(URI.parse(v))
+      File.new('test.json', "w+").syswrite(url_response)
+      puts "------#{k}-----"
+      js_response = JSON.parse(File.read('test.json'))
+      # puts js_response.class
+      js_response.each do |a,b|
+        if b.to_s["a"]
+          puts "Key: #{a}"
+        end
+      end
+    }
+    sleep 10
+    ss.join
+  end
 end
 test
